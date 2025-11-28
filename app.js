9d5f0667a0c78dc97170672b20e8f25c75914a27
ketkar-atharva <atharvaketkar13@gmail.com>
2025-11-28 20:41:54 +0530

    added navbar,footer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let app=express()
 let mongoose=require('mongoose');
 const Task=require('./models/task.js');
 let path=require("path");
+let methodoverride=require("method-override");
+let ejsmate=require("ejs-mate");
 
 main().then(()=>{
     console.log("connected to DB");
@@ -16,6 +18,10 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodoverride("_method"));
+app.engine("ejs",ejsmate);
+app.use(express.static(path.join(__dirname,"public")));
+
 //trial
 app.get("/",(req,res)=>{
     res.send("Hi i am root");
@@ -52,6 +58,20 @@ app.get("/tasks/delete/:id",async(req,res)=>{
    await Task.findByIdAndDelete(id);
    res.redirect("/tasks");
 })
+//edit api
+app.get("/tasks/edit/:id",async(req,res)=>{
+     let {id}=req.params;
+     let edittask=await Task.findById(id);
+     res.render("tasks/edit",{edittask});
+});
+
+//update api
+
+app.put("/update/:id",async(req,res)=>{
+    let{id}=req.params;
+    await Task.findByIdAndUpdate(id,req.body.tasks);
+    res.redirect("/tasks");
+});
 
 
 
